9a380203b261adfc5e328f81efba14f8661f8bd6
Md. Mostafizur Rahman <dev.mostafizur@gmail.com>
2020-10-06 16:48:15 +0600

    firebase auth implemented and sign-in, sign-out applied

--- src/App.js
+++ src/App.js
@@ -1,24 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import * as firebase from "firebase/app";
+import "firebase/auth";
+import firebaseConfig from './firebase.config';
+
+firebase.initializeApp(firebaseConfig);
 
 function App() {
+  const [user, setUser] = useState({
+    isSignedIn: false,
+    displayName: '',
+    email: '',
+    photoURL: ''
+  });
+
+  const provider = new firebase.auth.GoogleAuthProvider();
+  const handleClick = () => {
+    //console.log('Clicked here!');
+    firebase.auth().signInWithPopup(provider)
+    .then( res => {
+      //console.log(res);
+      const {displayName, email, photoURL} = res.user;
+      const signedInUser = {
+        isSignedIn: true,
+        name: displayName, 
+        email: email,
+        photo: photoURL
+      }
+      setUser(signedInUser);
+      console.log(displayName, email, photoURL); 
+    })  
+    .catch( err => {
+      console.log(err);
+      console.log(err.message);
+    });  
+  }
+
+  const handleSignOut = () => {
+    firebase.auth().signOut()
+    .then( res => {
+      const signedOutUser ={
+        isSignedIn: false,
+        name: '',
+        photo: '',
+        email: ''
+      }  
+      setUser(signedOutUser);   
+      //console.log(res);
+    })
+    .catch( () => {
+
+    });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App">      
+      {
+        user.isSignedIn ? <button onClick={handleSignOut}> Sign out</button>:        
+        <button onClick={handleClick}> Sign In</button>      
+      }
+      {
+        user.isSignedIn && <div> 
+            <p> Welcome, {user.name}</p>
+            <p> Email: {user.email}</p>
+            <img src={user.photo} alt=""/>
+          </div>
+      }
     </div>
   );
 }
